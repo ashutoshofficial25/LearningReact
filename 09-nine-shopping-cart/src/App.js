8deae202783b62ee837be2d36f5a41bc31c93a29
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-08-16 11:34:59 +0530

    cart section added

--- 09-nine-shopping-cart/src/App.js
+++ 09-nine-shopping-cart/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import { toast } from "react-toastify";
+
+import { Container, Row, Col } from "reactstrap";
+import { toast, ToastContainer } from "react-toastify";
 import BuyPage from "./Components/BuyPage";
+import Cart from "./Components/Cart";
 
 function App() {
   const [cartItem, setCartItem] = useState([]);
@@ -34,9 +36,22 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <BuyPage addInCart={addInCart} />
-    </div>
+    <Container>
+      <ToastContainer />
+      <Row>
+        <Col md="8">
+          <BuyPage addInCart={addInCart} />
+        </Col>
+        <Col md="4">
+          {" "}
+          <Cart
+            cartItem={cartItem}
+            removeItem={removeItem}
+            buyNow={buyNow}
+          />{" "}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
